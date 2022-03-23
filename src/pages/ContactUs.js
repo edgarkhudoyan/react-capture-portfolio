29278182f25acba57c12b809e9d108f53393c503
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-03-24 02:11:35 +0400

    Add basic animation to pages

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,5 +1,19 @@
+// Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
+
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <motion.div
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+      style={{ background: '#fff' }}
+    >
+      <h1>hey</h1>
+    </motion.div>
+  );
 };
 
 export default ContactUs;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -2,6 +2,9 @@ import { MovieState } from '../MovieState';
 import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+// Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
 
 const MovieDetail = () => {
   const history = useLocation();
@@ -19,7 +22,12 @@ const MovieDetail = () => {
   return (
     <>
       {movie && (
-        <Details>
+        <Details
+          variants={pageAnimation}
+          initial='hidden'
+          animate='show'
+          exit='exit'
+        >
           <Headline>
             <h2>{movie.title}</h2>
             <img src={movie.mainImg} alt='' />
@@ -54,7 +62,7 @@ const Award = ({ title, description }) => {
 };
 
 // Styled Components
-const Details = styled.div`
+const Details = styled(motion.div)`
   color: white;
 `;
 
