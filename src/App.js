0947851ecfd0e63b9b775ff9c60196be556786be
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-03-25 13:14:07 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 // Global Style
-import GlobalStyle from './components/GlobalStyle';
+import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
-import Nav from './components/Nav';
-import AboutUs from './pages/AboutUs';
-import OurWork from './pages/OurWork';
-import ContactUs from './pages/ContactUs';
-import MovieDetail from './pages/MovieDetail';
+import Nav from "./components/Nav";
+import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetail";
 // Router
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from "react-router-dom";
 // Animation
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   const location = useLocation();
 
   return (
-    <div className='App'>
+    <div className="App">
       <GlobalStyle />
       <Nav />
       <AnimatePresence exitBeforeEnter>
         <Routes location={location} key={location.pathname}>
-          <Route path='/' element={<AboutUs />} />
-          <Route path='/work' element={<OurWork />} />
-          <Route path='/work/:id' element={<MovieDetail />} />
-          <Route path='/contact' element={<ContactUs />} />
+          <Route path="/" element={<AboutUs />} />
+          <Route path="/work" element={<OurWork />} />
+          <Route path="/work/:id" element={<MovieDetail />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </AnimatePresence>
     </div>
